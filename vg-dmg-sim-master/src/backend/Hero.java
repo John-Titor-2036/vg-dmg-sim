7228c8d3485bf9645999f441802d6e21857e712c
Degaulle Dai <degaulledai@gmail.com>
2016-04-16 05:21:55 -0500

    basically only changed hero class.
    none of the other classes work with my new revamped hero class btw, but check it out and see what i did. i can explain things if you want, when i wake up

---
+++ vg-dmg-sim-master/src/backend/Hero.java
@@ -0,0 +1,134 @@
+package backend;
+
+import java.util.ArrayList;
+/*
+
+Authors: Jerry Zhou, Degaulle Dai
+
+ */
+
+public class Hero implements Abilities
+{
+    private String name;
+
+    private final int MAX_LEVEL = 11;
+    private double health;
+    private double fullHealth;
+    private double healthRegen;
+    private double energy;
+    private double fullEnergy;
+    private double energyRegen;
+    private double weaponDmg;
+    private double atkSpeed;
+    private double armor;
+    private double shield;
+
+    ArrayList<Buff> buffs = new ArrayList<Buff>();
+
+    /**
+     * constructor
+     */
+    public Hero(String name, int lvl,  int weaponDmg, int maxWeaponDmg, int health, int maxHealth, int energy, int maxEnergy, int armor, int maxArmor, int shield, int maxShield, double atkSpeed, double maxAtkSpeed, int healthRegen, int maxHealthRegen)
+    {
+        this.name = name;
+        this.weaponDmg = weaponDmg + (maxWeaponDmg-weaponDmg)/MAX_LEVEL * lvl;
+        this.health = health + (maxHealth-health)/MAX_LEVEL * lvl;
+        this.energy = energy + (maxEnergy-energy)/MAX_LEVEL * lvl;
+        this.armor = armor + (maxArmor-armor)/MAX_LEVEL * lvl;
+        this.shield = shield + (maxShield-shield)/MAX_LEVEL * lvl;
+        this.atkSpeed = atkSpeed + (maxAtkSpeed-atkSpeed)/MAX_LEVEL * lvl;
+        fullEnergy = this.energy;
+        fullHealth = this.health;
+    }
+
+    /**
+     * gets stats
+     */
+    public String getName(){ return name; }
+    public double getHealth(){ return health; }    
+    public double getEnergy(){ return energy; }
+    public double getWeaponDmg(){ return weaponDmg; }
+    public double getArmor(){ return armor; }
+    public double getShield(){ return shield; }
+    public double getAtkSpeed(){ return atkSpeed; }
+
+    /**
+     * changes stats
+     */
+    public void setHealth(int health){this.health = health;}
+    public void setEnergy(int energy){ this.energy = energy; }
+    public void setArmor(int armor){ this.armor = armor; }
+    public void setShield(int shield){ this.shield = shield; }
+    public void setAtkSpeed(double atkSpd){ this.atkSpeed = atkSpd; }
+
+    /** Type 1 = Weapon Damage
+     *Type 2 = Crystal Damage
+     *Type 3 = Crittable Weapon Damage
+     *Type 4 = Basic Attack Weapon Component
+     *Type anything else = True Damage
+     */
+    public int calculateDamage(int type, int damage, double pierce)
+    {
+        int damageTaken = 0;
+        int critRng;
+        int ratio = 1;
+        int critChance =1;
+        int critDmg = 1;
+        if(type == 1)
+        {
+            damageTaken = (int) ( (damage*pierce*ratio)+((damage*ratio*(1-pierce))/(1+(0.01*(armor)))) );
+        }
+        else if (type == 2)
+        {
+            damageTaken =(int) ( (damage*pierce*ratio)+((damage*ratio*(1-pierce))/(1+(0.01*(shield)))) );
+        }
+        else if (type == 3)
+        {
+            critRng = (int)(Math.random()*1.01);
+            if (critRng<= critChance)
+                damageTaken =(int) ( (damage*critDmg*pierce*ratio) + ((damage*critDmg*(1-pierce))/(1+(0.01*(armor)))) );
+            else
+                damageTaken =(int) ( (damage*pierce*ratio) + ((damage*(1-pierce))/(1+(0.01*(armor)))) );
+        }
+        else if (type == 4)
+        {
+            critRng = (int)(Math.random()*1.01);
+            if (critRng<= critChance)
+                damageTaken =(int) ( ((weaponDmg+damage)*critDmg*pierce) + (((weaponDmg+damage)*critDmg*(1-pierce))/(1+(0.01*(armor)))) );
+            else
+                damageTaken =(int) ( ((weaponDmg+damage)*pierce) + (((weaponDmg+damage)*(1-pierce))/(1+(0.01*(armor)))) );
+        }
+        else
+        {
+            damageTaken = damage;
+        }
+        return damageTaken;
+    }
+    
+    
+    //Buffs functions
+    public void applyBuff(String type, double effect, int time){
+        buffs.add(new Buff(this, type, effect, time));
+    }    
+    public void manageBuffs(){
+        for(int i=0; i<buffs.size(); i++){
+            Buff buff = buffs.get(i);
+            if(buff.getTime() <= 0) buffs.remove(i);
+            else buff.tick();
+        }
+    }
+    
+
+    public int firstAbility() {
+        return 0;
+    }
+    public int secondAbility() {
+        return 0;
+    }
+    public int ultimateAbility() {
+        return 0;
+    }
+    public boolean onCd(int ability) {
+        return false;
+    }
+} 
